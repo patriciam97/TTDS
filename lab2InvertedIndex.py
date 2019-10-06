3348803f96b793f040c9df524abf56ccffd9de48
Patricia milou <patriciamilou97@gmail.com>
2019-10-06 16:09:03 +0100

    working on lab2

--- lab2InvertedIndex.py
+++ lab2InvertedIndex.py
@@ -5,36 +5,77 @@ from collections import OrderedDict
 
 stop = []
 index = {}
+documents = []
 collections =["sample","trec.sample"]
-def readStopWords():
+
+def read_stop_words():
     f = open ("stopWords.txt")
     for x in f:
         stop.append(x.strip())
-def readSamples():
+
+def read_samples():
     global collections
     for collection in collections:
         f = open("collections/"+collection+".txt")
         for x in f:
             if "ID:" in x:
-                id_var = x.split(":")[1].strip()
+                doc_id = x.split(":")[1].strip()
             else:
                 text = x.split(":")[1]
-                processText(id_var,text)
-        saveIndex(collection)
+                process_text(doc_id,text)
+        save_inverted_index(collection)
         global index
-        index={}
-def buildInvertedIndex(id_var,words):
+
+def build_inverted_index ():
+    read_samples()
+    
+def generate_index(doc_id,words): 
+    # words are processed - tokenize, stopping and stemming
     for i,word in enumerate(words,1):
         if word not in index:
             # first push
-            index[word] = {id_var:[i]}
+            index[word] = {doc_id:[i]}
         else:
             # more push
             documents = index[word].keys()
-            if id_var not in documents:
-                index[word][id_var]=[i]
-            else: index[word][id_var].append(i)
-def processText(id_var,text):
+            if doc_id not in documents:
+                index[word][doc_id]=[i]
+            else: index[word][doc_id].append(i)
+
+def sub_array(arr,start,n): 
+    # from start get n next elements
+    result = []
+    for i in list(range(start,start+n)):
+        result.append(arr[i])
+    return result
+
+def process_text_for_n_gram(doc_id,text,n):
+    text = text.split()
+    arr = []
+    processedPhrases = []
+    for i,word in enumerate(text,0):
+        if i<(len(text)-n+1):
+            arr.append(sub_array(text,i,n))
+    for pair in arr:
+        phrase = "_".join(pair)
+        processedPhrases.append(phrase)
+    generate_index(doc_id,processedPhrases)
+    return processedPhrases
+    
+def build_n_gram(n):
+    # words are unprocessed
+    global collections
+    for collection in collections:
+        f = open("collections/"+collection+".txt")
+        for x in f:
+            if "ID:" in x:
+                doc_id = x.split(":")[1].strip()
+            else:
+                text = x.split(":")[1]
+                process_text_for_n_gram(doc_id,text,n)
+        save_n_gram_index(collection,n)
+
+def process_text(doc_id,text):
     processedWords= []
     unprocessedWords = 0
     unique = {}
@@ -49,19 +90,25 @@ def processText(id_var,text):
         if token not in stop:
             if stem(token).strip() != "":
                 processedWords.append(stem(token).strip())
-    buildInvertedIndex(id_var,processedWords)
-def saveIndex(title):
-    title= title+"Index.txt"
+    generate_index(doc_id,processedWords)
+
+def save_inverted_index(title):
+    title= "indexes/"+title+"_Inverted_Index.txt"
     f = open(title,"w+")
     for word in index:
         f.write("%s:\n" % word)
         for doc in index[word]:
             f.write("\t%s: %s\n"%(doc,",".join(str(x) for x in index[word][doc])))
-def readIndex(title):
-    title = title+"Index.txt"
-    f = open(title)
-    index = {}
-    documents = []
+
+def save_n_gram_index(title,n):
+    title= "indexes/"+title+"_"+str(n)+"_Gram.txt"
+    f = open(title,"w+")
+    for word in index:
+        f.write("%s:\n" % word)
+        for doc in index[word]:
+            f.write("\t%s: %s\n"%(doc,",".join(str(x) for x in index[word][doc])))
+
+def read_index(f):
     for line in f:
         if(line.split(":")[1].strip()==""):
             word = line.split(":")[0]
@@ -79,8 +126,23 @@ def readIndex(title):
                     index[word][document].append(pos)
     return index,documents
 
+def read_inverted_index(title):
+    title = "indexes/"+title+"_Inverted_Index.txt"
+    f = open(title)
+    print(title+":")
+    return read_index(f)
+        
+
+def read_n_gram_index(title,n):
+    title = "indexes/"+title+"_"+str(n)+"_Gram.txt"
+    f = open(title)
+    print(title+":")
+    return read_index(f)
+
 def main():
-    readSamples()
+    build_inverted_index()
+    build_n_gram(2)
+
 if __name__ == "__main__" :
     main()
 
--- lab2Modules.py
+++ lab2Modules.py
@@ -1,59 +1,76 @@
 import sys
 import lab2InvertedIndex as indexes
-collections =["sample","trec.sample"]
+from stemming.porter2 import stem
 
-def andOperator(var1,var2,index):
+collections =["sample","trec.sample"]
+operators = []
+sets = []
+
+def and_operator(var1,var2,index):
     if (type(var1)!=list) & (type(var2)!=list):
-        if (var1 not in index) and (var2 not in index):
-            return None
+        if (var1 not in index) or (var2 not in index):
+            return []
         else:
             return list(set(index[var1].keys()) & set(index[var2].keys()))
     elif (type(var1) == list) & (type(var2) ==  str):
-        print("here")
-        print(list(set(var1) & set(index[var2].keys())))
         if (var2 not in index):
-            return None
+            return []
         else:
             return list(set(var1) & set(index[var2].keys()))
     elif (type(var2) == set) & (type(var1) ==  str):
         if (var1 not in index):
-            return None
+            return []
         else:
             return list(set(var2) & set(index[var1].keys()))
     else:
         return list(set(var1) & set(var2))
-def orOperator(var1,var2,index):
+
+def or_operator(var1,var2,index):
     if (type(var1)!=list) & (type(var2)!=list):
         if (var1 not in index) and (var2 not in index):
-            return None
+            return []
+        elif (var1 in index) and (var2 not in index):
+            return list(set(index[var1].keys()))
+        elif (var1 not in index) and (var2 in index):
+            return list(set(index[var2].keys()))
         else:
             return list(set(index[var1].keys()) | set(index[var2].keys()))
     elif (type(var1) == list) & (type(var2) ==  str):
         if (var2 not in index):
-            return None
+            return []
         else:
             return list(set(var1) | set(index[var2].keys()))
     elif (type(var2) == set) & (type(var1) ==  str):
         if (var1 not in index):
-            return None
+            return []
         else:
             return list(set(var2) | set(index[var1].keys()))
     else:
         return list(set(var1) | set(var2))
         
-def notOperator(var,index,documents):
+def not_operator(var,index,documents):
     if (var in index):
         return list(set(documents) ^ set(index[var].keys()))
     else:
         return None
 
-def findMatches(arguments):
-    index,documents = indexes.readIndex("sample")
+def find_phrase (sets):
+    phrase = "_".join(sets)
+    if (len(sets)==2):
+        for collection in collections:
+            index,documents = indexes.read_n_gram_index(collection,2)
+            print(collection+":"+index[phrase].keys())
+        else:
+            indexes.build_n_gram(len(sets))
+            for collection in collections:
+                index,documents = indexes.read_n_gram_index(collection,len(sets))
+                print(collection+":"+index[phrase].keys())
+
+def set_up_sets_and_operators(arguments):
     oper_types = ["AND","OR","NOT","OR NOT","AND NOT"]
-    operators = []
-    sets = []
     previous = False
     for x in arguments:
+        x = stem(x)
         if x in oper_types:
             if (x == "AND"):
                 previous = "AND"
@@ -73,6 +90,7 @@ def findMatches(arguments):
                 elif previous == "OR":
                     previous = False                    
                     operators.append("OR")
+
                 operators.append(x)
         else: 
             sets.append(x)
@@ -83,30 +101,55 @@ def findMatches(arguments):
                 elif previous == "OR":
                         previous = False                    
                         operators.append("OR")
-    while (len(operators)!=0):
-        print("sets:",sets,"opers:",operators)
-        oper = operators.pop(0)
-        print("operatorrrr:",oper)
-        if (oper == "NOT"):
-            var = sets.pop(0)
-            docs = notOperator(var,index,documents)
-            # sets.insert(0,[docs])
-            sets.append(list(docs))
-        elif (oper == "AND NOT"):
-            sets.insert(0,andOperator(sets.pop(0),notOperator(sets.pop(0),index,documents),index))
-        elif (oper == "OR NOT"):
-            sets.insert(0,orOperator(sets.pop(0),notOperator(sets.pop(0),index,documents),index))
-        elif (oper == "AND"):
-            sets.insert(0,andOperator(sets.pop(0),sets.pop(0),index))
-        elif (oper == "OR"):
-            sets.insert(0,orOperator(sets.pop(0),sets.pop(0),index))
-    return (sets)
+
+def find_matches(arguments):
+    global sets,operators
+    set_up_sets_and_operators(arguments)
+    print("sets:",sets,"opers:",operators)
+
+    if (len(operators) == 0 and len(sets)>1): 
+        # phrase
+        return find_phrase(sets)
+    elif (len(operators) == 0 and len(sets)==1):
+        # just one word
+        for collection in collections:
+            index,documents = indexes.read_inverted_index(collection)
+            if (sets[0] in index):
+                print (index[sets[0]].keys())
+            else: 
+                print([])
+    else:
+        # logical expression
+        for collection in collections:
+            sets_copy=sets.copy()
+            operators_copy = operators.copy()
+            index,documents = indexes.read_inverted_index(collection)
+            while (len(operators_copy)!=0):
+                oper = operators_copy.pop(0)
+                if (oper == "NOT"):
+                    var = sets.pop(0)
+                    docs = not_operator(var,index,documents)
+                    sets_copy.append(list(docs))
+                elif (oper == "AND NOT"):
+                    sets_copy.insert(0,and_operator(sets_copy.pop(0),not_operator(sets_copy.pop(0),index,documents),index))
+                elif (oper == "OR NOT"):
+                    sets_copy.insert(0,or_operator(sets_copy.pop(0),not_operator(sets_copy.pop(0),index,documents),index))
+                elif (oper == "AND"):
+                    sets_copy.insert(0,and_operator(sets_copy.pop(0),sets_copy.pop(0),index))
+                elif (oper == "OR"):
+                    sets_copy.insert(0,or_operator(sets_copy.pop(0),sets_copy.pop(0),index))
+            print(sets_copy)
 
 
 def main(arguments):
-    if arguments[1] == "FIND":
-        for x in findMatches(arguments[2:]):
-            print (x)
+    while True:
+        global operators,sets
+        operators = []
+        sets = []
+        query= input("Search for: ")
+        find_matches(query.split())
+        # for x in find_matches(query.split()):
+        #     print (x)
     
 if __name__ == "__main__" :
     main(sys.argv)
