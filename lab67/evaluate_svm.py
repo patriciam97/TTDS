f7f47483ff6531cdb41676d8176e166dcdebe01a
Patricia milou <patriciamilou97@gmail.com>
2019-11-04 13:37:38 +0000

    evaluation on svm

--- lab67/evaluate_svm.py
+++ lab67/evaluate_svm.py
@@ -2,27 +2,31 @@ from sklearn.metrics import multilabel_confusion_matrix
 
 def read_test_pred():
     catg_true, catg_pred =[],[]
-    with open("feats.test", encoding="utf8", errors='ignore') as f:
-        for line in f.readlines():
+    with open("feats.test", encoding="utf8", errors='ignore') as t:
+        for line in t.readlines():
             true_catg = line.split(" ")[0]
             catg_true.append(true_catg)
-    with open("pred.out", encoding="utf8", errors='ignore') as f:
-        for line in f.readlines():
+    t.close()
+    with open("pred.out", encoding="utf8", errors='ignore') as p:
+        for line in p.readlines():
             pred_catg = line.split(" ")[0]
             catg_pred.append(pred_catg)
+    p.close()
     return catg_true, catg_pred
 
 def calculate_statistics(catg_true,catg_pred):
-    acc = 0
+    accuracies,precisions,recalls,f1 = [],[],[],[]
     for i,category in enumerate(multilabel_confusion_matrix(catg_true,catg_pred),1):
-        tp,fp = category[0]
-        tn,fn = category[1]
-        acc += (tp+tn)/(tn + fp + fn + tp )
-        pres = tp/(tp+fp)
-        recall = tp/(tp+fn)
-        f1 = (2*pres*recall)/(pres+recall)
-        print(str(i)+":	P="+str(pres)+" R="+str(recall)+" F="+str(f1)) 
-    print(acc)
+        (tn,fp),(fn,tp) = category
+        accuracies.append((tp+tn)/(tn + fp + fn + tp ))
+        precisions.append(tp/(tp+fp))
+        recalls.append(tp/(tp+fn))
+        f1.append((2*float(precisions[-1])*float(recalls[-1]))/((float(precisions[-1])+float(recalls[-1]))))
+    print(accuracies)
+    print("Accuracy: " + str(sum(accuracies)/len(accuracies)))
+    for i,pres in enumerate(precisions):
+        print(str(i+1)+":	P="+str(pres)+" R="+str(recalls[i])+" F="+str(f1[i])) 
+
     
 def main():
     catg_true,catg_pred = read_test_pred()
