08578962919afb7b08f7c37feb920b718866ebb9
Patricia milou <patriciamilou97@gmail.com>
2019-11-16 11:12:36 +0000

    eval_svm fixed

--- lab67/evaluate_svm.py
+++ lab67/evaluate_svm.py
@@ -1,4 +1,5 @@
-from sklearn.metrics import multilabel_confusion_matrix
+from sklearn.metrics import confusion_matrix,multilabel_confusion_matrix
+from collections import OrderedDict
 
 def read_test_pred():
     catg_true, catg_pred =[],[]
@@ -14,20 +15,43 @@ def read_test_pred():
     p.close()
     return catg_true, catg_pred
 
+def calculateConfusionMatrix(distinctClasses, testClassesList, predictionClassesList):
+    """Calculates the confusion matrix of the classifier
+    Returns
+    -------
+    confusionMatrix : Dictionary type
+        Confusion matrix - Dictionary of Dictionaries and frequencies
+    """
+    confusionMatrix = OrderedDict()
+    for index in distinctClasses:
+        confusionMatrix[index] = OrderedDict()
+        for innerIndex in distinctClasses:
+            confusionMatrix[index][innerIndex] = 0
+
+    totalPredictions = len(testClassesList)
+    for i in range(0, totalPredictions):
+        confusionMatrix[int(testClassesList[i])][int(predictionClassesList[i])] += 1
+
+    return confusionMatrix
+    
 def calculate_statistics(catg_true,catg_pred):
     accuracies,precisions,recalls,f1 = [],[],[],[]
-    for i,category in enumerate(multilabel_confusion_matrix(catg_true,catg_pred),1):
-        (tn,fp),(fn,tp) = category
-        accuracies.append((tp+tn)/(tn + fp + fn + tp ))
-        precisions.append(tp/(tp+fp))
-        recalls.append(tp/(tp+fn))
-        f1.append((2*float(precisions[-1])*float(recalls[-1]))/((float(precisions[-1])+float(recalls[-1]))))
+    print( multilabel_confusion_matrix(catg_true,catg_pred))
+    # for i,category in enumerate(calculateConfusionMatrix(list(range(1,15)),catg_true,catg_pred),1):
+    for i,category in enumerate(multilabel_confusion_matrix(catg_true,catg_pred,labels = list((range(1,15)))),1):
+        tn, fp, fn, tp = category.ravel()
+        accuracies.append((tp)/(tn + fp + fn + tp ))
+        pres = tp/(tp+fp)
+        precisions.append(pres)
+        recall = tp/(tp+fn)
+        recalls.append(recall)
+        f1.append((2*float(pres)*float(recall))/((float(pres)+float(recall))))
     save_statistics(accuracies,precisions,recalls,f1)
    
 def save_statistics(accuracies,precisions,recalls,f1):
     file_Title= "Eval2.txt"
     f = open(file_Title,"w+")  
-    f.write("Accuracy: " + str(round(sum(accuracies)/len(accuracies),3))+"\n")
+    f.write("Accuracy: " + str(round(sum(accuracies),3))+"\n")
     f.write("Macro-F1: " + str(round(sum(f1)/len(f1),3))+"\n")
     for i,pres in enumerate(precisions):
         f.write(str(i+1)+":\tP="+str(round(pres,3))+"\tR="+str(round(recalls[i],3))+"\tF="+str(round(f1[i],3))+"\n") 
